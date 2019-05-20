7a05ff00c1d1220ef59e8bbb134e5b8f29a3429c
OgeeManta <kondorkakas@gmail.com>
2019-05-20 10:26:52 +0200

    Added Talent Tree Display scrap

---
+++ src/view/TalentDisplay.java
@@ -0,0 +1,27 @@
+package view;
+
+import javax.swing.*;
+
+public class TalentDisplay {
+    private JPanel talentRootPanel;
+    private JPanel offensePanel0;
+    private JButton offenseButton0;
+    private JPanel defensePanel0;
+    private JButton defenseButton0;
+    private JPanel utilityPanel0;
+    private JButton utilityButton0;
+    private JPanel defensePanel1;
+    private JButton defenseButton1;
+    private JPanel offensePanel1;
+    private JButton offenseButton1;
+    private JPanel offensePanel2;
+    private JButton offenseButton2;
+    private JPanel defensePanel2;
+    private JButton defenseButton2;
+    private JPanel utilityPanel2;
+    private JButton utilityButton2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
